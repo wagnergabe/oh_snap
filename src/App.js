30204c7dcc07cc83ae95f13ebd77da072f01d609
GSvenny <gabrielswagner7@gmail.com>
2022-09-03 20:58:12 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import About from './components/About';
+
+function App() {
+  return (
+   <div>
+    <main>
+      <About></About>
+    </main>
+  </div>
+  );
+}
+
+export default App;
