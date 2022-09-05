6b19689379bd6c04ac2c9bc5971f958d77dfc6a0
GSvenny <gabrielswagner7@gmail.com>
2022-09-04 20:30:29 -0500

    added test to About component

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    //renders About test
+    it('renders', () => {
+        render(<About />);
+    });
+    it('matches snapshot DOM node structre', () => {
+        const { asFragment } =render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+  })
